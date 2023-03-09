170bba0d8ce85007a40a6bcbe43c54f4ead4cf62
akashp1861 <akashanilpatil2000@gmail.com>
2023-03-09 11:47:07 +0530

    my first commit

---
+++ src/BasicsOfJava/objective_type.java
@@ -0,0 +1,18 @@
+package BasicsOfJava;
+
+public class objective_type {
+	
+	public static void main(String[] args) {
+		String s1 = "Ak@sh123";
+		System.out.println("Original String is -  "+s1);
+		
+	 for (int i=0; i<=s1.length(); i++) {
+		 
+		 if (Character.isAlphabetic(s1.charAt(i)));
+		 
+		 
+	 }
+  
+	
+	}
+}
